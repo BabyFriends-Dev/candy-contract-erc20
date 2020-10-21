df551245ae5b8633a573720d01bf078a089c6d7a
mech12 <whdnrfo@gmail.com>
2020-10-21 17:26:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# candy-contracts
+
+nvm use v9.5.0
+
+
+Smart-contracts for Cryto Candy platform
+
+## Deployment - Ethereum
+
+1. First uncomment the deployer function corresponds to the contract you want to deploy and comment out all the others in `migrations/2_candytoken.js` file.
+2. If the project folder includes `build` folder, first delete it
+3. Compile the corresponding contract as follows;
+   `truffle compile`
+4. Set mnemonic words for deployer in your command line as follows;
+   `export MNEMONICS="<mnemonic_words>"`
+5. And set your infura project secret key as follows;
+   `export INFURA_API_KEY="<infura_project_secret>"`
+6. Also set fund collector address as follows;
+   `export FUND_COLLECTOR_ADDRESS="<fund_collector_address>"`
+7. Finally deploy the contract on the network you desire
+   `NETWORK=<network_name> npm run deploy`
+
+```bash
+NETWORK=ropsten truffle compile
+NETWORK=ropsten npm run deploy
+NETWORK=ropsten truffle deploy --network ropsten  --reset
+
+```
+
+## Deploy 인자 
+
+: name , symbol , decimals, initialSupply
+: "CRYPTO CANDY" , "CANDY" , 18 , 800000000
+
+
+## Test
+
+- In order to run the whole tests
+  `truffle test`
+- In order to run only specific test file
+
+## mainnet candy token address
+
+https://etherscan.io/token/0x540D3087B21F31F9A810385c94627a067cfD0b08
---
+++ migrations/2_candytoken.js
@@ -0,0 +1,125 @@
+const CandyToken = artifacts.require("./CandyToken.sol");
+const CandyCrowdsale = artifacts.require("./CandyCrowdsale.sol");
+const CandyAirdrop = artifacts.require("./CandyAirdrop.sol");
+
+const name = "CRYPTO CANDY";
+const symbol = "CANDY";
+const decimals = 18;
+const initialSupply = 800000000; // 9??
+
+// TODO: Change this parameters in mainnet deployment
+const rate = 21935; // 1 ETH = 21,935.9375 CANDY token
+const totalSaleCap = 2279 * Math.pow(10, 18); // 2,279.36463 ETH
+const wallet = process.env.FUND_COLLECTOR_ADDRESS;
+
+/************* CANDY Token deployed information ***************/
+
+// Mainnet - Ethereum
+// @see https://etherscan.io/token/0x540d3087b21f31f9a810385c94627a067cfd0b08
+const CANDY_TOKEN_ADDRESS_MAINNET =
+  "0x540d3087b21f31f9a810385c94627a067cfd0b08";
+
+
+// Deployer
+const TokenContractDeployer = (deployer, network) => {
+  if (
+    network === "ropsten" ||
+    network === "baobab" || // TestNet
+    network === "mainnet" ||
+    network === "cypress"
+  ) {
+    // MainNet
+    deployer
+      .deploy(CandyToken, name, symbol, decimals, initialSupply)
+      .then((_) =>
+        console.log(
+          "CANDY Token contract has been deployed successfully. CandyToken.address =" +
+            CandyToken.address
+        )
+      );
+  } else {
+    throw new Error("Unknown network!");
+  }
+};
+
+/*************************************************************/
+
+/************* CANDY Crowdsale deployed information ***************/
+
+// Mainnet - Ethereum
+// @see https://etherscan.io/0x
+// const CANDY_CROWDSALE_ADDRESS = '';
+
+// Ropsten - Ethereum
+// @see https://rinkeby.etherscan.io/0xb0eb24ce9b029a9e771a7878c7983e1d06f5895d
+// const CANDY_CROWDSALE_ADDRESS = '0xe9b029a9e771a7878c7983e1d06f5895db0eb24c';
+
+// Baobab - Klaytn
+// const CANDY_CROWDSALE_ADDRESS = '0x69f75e30ee83a450b033fd6451fd7993051bb19c';
+
+// Deployer
+const SaleContractDeployer = (deployer, network) => {
+  deployer
+    .deploy(
+      CandyCrowdsale,
+      rate,
+      wallet,
+      getTokenAddress(network),
+      totalSaleCap
+    )
+    .then((_) =>
+      console.log(
+        `CANDY Crowdsale contract has been deployed successfully on ${network}.`
+      )
+    );
+};
+
+function getTokenAddress(network) {
+  switch (network) {
+    case "mainnet":
+      return CANDY_TOKEN_ADDRESS_MAINNET;
+    case "ropsten":
+      return CANDY_TOKEN_ADDRESS_ROPSTEN;
+    case "baobab":
+      return CANDY_TOKEN_ADDRESS_BAOBAB;
+    case "cypress":
+      return CANDY_TOKEN_ADDRESS_CYPRESS;
+    default:
+      throw new Error("Unknown network!");
+  }
+}
+
+/************* CANDY Airdrop deployed information ***************/
+
+// mainnet
+// const CANDY_AIRDROP_ADDRESS = '0x84b148d389a94bf97abba8bf04bc4b0f33355418';
+
+// Deployer
+const AirdropContractDeployer = (deployer, network) => {
+  deployer
+    .deploy(CandyAirdrop, getTokenAddress(network))
+    .then((_) =>
+      console.log(
+        `CANDY Airdrop contract has been deployed successfully on ${network}.`
+      )
+    );
+};
+
+/*****************************************************************/
+
+module.exports = (deployer, network) => {
+  /**
+   * Token contract deploy.
+   */
+  TokenContractDeployer(deployer, network);
+
+  /**
+   * Sale contract deploy.
+   */
+  // SaleContractDeployer(deployer, network);
+
+  /**
+   * Sale contract deploy.
+   */
+  // AirdropContractDeployer(deployer, network);
+};
